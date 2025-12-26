b33339f14d20e5d409553b0d628a6e1325f12d32
Elizabeth Peter <epeter@umass.edu>
2025-12-26 11:57:53 +0530

    Initial commit: React + TypeScript setup

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div style={{ padding: "2rem" }}>
+      <h1>Study Group Matchmaker</h1>
+      <p>Find your perfect study group.</p>
+    </div>
+  );
+}
+
+export default App;
+
